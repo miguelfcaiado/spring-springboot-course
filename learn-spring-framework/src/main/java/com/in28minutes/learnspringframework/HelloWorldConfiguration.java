ace1b24b6a868b5239d59b7d394419c4996a9d1e
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-06-24 11:43:13 +0100

    Using spring beans

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,49 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+// Released in JDK 16
+record Person(String name, int age, Address address) {};
+
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 
 	@Bean
 	public String name() {
 		return "Miguel";
+	}
+	
+	@Bean
+	public int age() {
+		return 26;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+	
+	//Auto-Wired by spring
+	@Bean
+	public Person person3Parameters(String name, int age, Address address2) {
+		return new Person(name, age, address2);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Baker Street", "London");
+	}
+	
+	// Bean can have a different name that is not the method name
+	@Bean(name = "address2")
+	public Address address2() {
+		return new Address("Belem", "Lisboa");
 	}
 }
