0c837148bbdd3d0e23ae7a9c9ed150953ab01ca8
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-06-24 13:19:20 +0100

    Working with spring beans and some annotations

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -1,5 +1,7 @@
 package com.in28minutes.learnspringframework;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
@@ -20,9 +22,19 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("person"));
 		System.out.println(context.getBean("person2MethodCall"));
 		System.out.println(context.getBean("person3Parameters"));
+		System.out.println(context.getBean("person4Parameters"));
+		System.out.println(context.getBean("person5Qualifier"));
 		
-		System.out.println(context.getBean("address"));
-		//System.out.println(context.getBean(Address.class)); // Other way to call beans
+		System.out.println(context.getBean("address1"));
+		System.out.println(context.getBean(Address.class)); // Other way to call beans. 
+		// Since we have the class Address used for more then one method we use the annotation @Primary to solve conflict 
+		
+		// Get a list of spring beans
+		String beanNames[] = context.getBeanDefinitionNames();
+		System.out.println(Arrays.toString(beanNames));
+		
+		// Tried this but I was getting an error
+		//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 // Released in JDK 16
@@ -39,12 +41,24 @@ public class HelloWorldConfiguration {
 	}
 	
 	@Bean
+	public Person person4Parameters(String name, int age, Address address) {
+		return new Person(name, age, address); //name, age, address1
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address2qualifier") Address address) {
+		return new Person(name, age, address); //name, age, address2
+	}
+	
+	@Bean(name = "address1")
+	@Primary
 	public Address address() {
 		return new Address("Baker Street", "London");
 	}
 	
 	// Bean can have a different name that is not the method name
 	@Bean(name = "address2")
+	@Qualifier("address2qualifier")
 	public Address address2() {
 		return new Address("Belem", "Lisboa");
 	}
