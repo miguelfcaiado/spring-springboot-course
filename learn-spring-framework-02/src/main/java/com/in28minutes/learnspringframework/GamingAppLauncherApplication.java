665b235a45f57a5e25be348d4c9fe0a3f4212b6d
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-06-25 21:34:09 +0100

    Getting spring to create and manage objects

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -1,15 +1,19 @@
 package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
 
-public class App03GamingSpringBeans {
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
+public class GamingAppLauncherApplication {
 
 	public static void main(String[] args) {
 		
-		try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+		try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
 			
 			context.getBean(GamingConsole.class).up();
 			
