fbd87e744203caee8fa1ef1ac18adbbc3bda6cf9
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-06-25 19:42:28 +0100

    Using Spring to manage Beans for gaming app

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,32 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+
+@Configuration
+public class GamingConfiguration {
+	
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+	
+	//var game = new MarioGame();
+	//var game = new SuperContra();
+//	var game = new PacmanGame();
+//	
+//	var gameRunner = new GameRunner(game);
+//	
+//	gameRunner.run();
+}
