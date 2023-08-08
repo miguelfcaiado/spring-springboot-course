034ed4a3084e3df6f6261c405ee85d2fa145904e
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-08-08 18:44:24 +0100

    Building 3 diferent hello world API

---
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.rest.webservices.resfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloworldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloworldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello World " + name);
+	}
+}
