cf99c2d71a55c523796b7a7b00e9cd67b797dce5
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-08-09 17:41:42 +0100

    Implementing REST API with validations and error handling

---
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/User.java
@@ -0,0 +1,54 @@
+package com.in28minutes.rest.webservices.resfulwebservices.user;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+public class User {
+	
+	private int id;
+	
+	@Size(min=2, message = "Name should have atleast 2 characters")
+	private String name;
+	
+	@Past(message = "Birth date should be in the past")
+	private LocalDate birthDate;
+	
+	public User(int id, String name, LocalDate birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+
+}
