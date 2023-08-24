04f41a83201fe11daeccf71514849e05d000cbd1
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-08-24 19:51:55 +0100

    Connect to H2 using JPA and Hibernate + creating post entity

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/User.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/User.java
@@ -1,14 +1,27 @@
 package com.in28minutes.rest.webservices.resfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 	
+	protected User() {
+		
+	}
+	
+	@Id
+	@GeneratedValue
 	private int id;
 	
 	@Size(min=2, message = "Name should have atleast 2 characters")
@@ -18,6 +31,10 @@ public class User {
 	@Past(message = "Birth date should be in the past")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> post;
 	
 	public User(int id, String name, LocalDate birthDate) {
 		super();
