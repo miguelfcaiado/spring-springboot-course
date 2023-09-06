a8b4e04dbd8074b6c55c337912f24376c1fba78f
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-09-06 17:43:01 +0100

    Getting started with react todo app

---
+++ first-npm-project/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
+export default function TodoApp() {
+    return (
+        <div className="TodoApp">
+            <LoginComponent />
+            {/* <WelcomeComponent /> */}
+        </div>
+    )
+}
+
+function LoginComponent() {
+
+    const [username, setUsername] = useState('in28minutes')
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    return (
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label>User Name:</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
+                </div>
+                <div>
+                    <button type="button" name="login">login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WelcomeComponent() {
+    return (
+        <div className="Login">
+            Welcome Component
+        </div>
+    )
+}
