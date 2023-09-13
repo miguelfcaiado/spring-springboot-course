2164c487f95ee1c1c2c47f22cca2eca9f6aea47e
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-09-13 16:32:22 +0100

    Displaying more todos details in react list todo component

--- first-npm-project/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ first-npm-project/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, useParams, Link } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -7,10 +7,11 @@ export default function TodoApp() {
         <div className="TodoApp">
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<LoginComponent />}></Route>
-                    <Route path='/login' element={<LoginComponent />}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent />}></Route>
-                    <Route path='*' element={<ErrorComponent />}></Route>
+                    <Route path='/' element={<LoginComponent />} />
+                    <Route path='/login' element={<LoginComponent />} />
+                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
+                    <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='*' element={<ErrorComponent />} />
                 </Routes>
             </BrowserRouter>
         </div>
@@ -39,7 +40,7 @@ function LoginComponent() {
         if(username === 'in28minutes' && password === 'dummy') {
             setShowSuccessMessage(true)
             setShowErrorMessage(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         }
         else {
             setShowSuccessMessage(false)
@@ -70,11 +71,14 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+    
+    const {username} = useParams()
+
     return (
         <div>
-            <h1>Welcome in28minutes</h1>
+            <h1>Welcome {username}</h1>
             <div className="WelcomeComponent">
-                Welcome Component
+                Manage your todos - <Link to="/todos">Go here</Link>
             </div>
         </div>
     )
@@ -86,6 +90,50 @@ function ErrorComponent() {
             <h1>We are working really hard!</h1>
             <div>
                 Apologies for the 404. React out to our team at ABC-DEF-GHIJ.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+    
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
+    
+    const todos = [
+                    {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
+                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
+                    {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate},
+                ]
+    
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things you want to do!</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(
+                                todo => (
+                                    <tr key={todo.id}>
+                                        <td>{todo.id}</td>
+                                        <td>{todo.description}</td>
+                                        <td>{todo.done.toString()}</td>
+                                        <td>{todo.targetDate.toDateString()}</td>
+                                    </tr>
+                                )
+                            )
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
