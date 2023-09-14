530943f8c7bc935c1ce55dd9be4c0df3d2a03f8c
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-09-14 17:00:40 +0100

    Adding header, footer and logout + styling with bootstrap

--- first-npm-project/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ first-npm-project/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -6,13 +6,16 @@ export default function TodoApp() {
     return (
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
                     <Route path='/' element={<LoginComponent />} />
                     <Route path='/login' element={<LoginComponent />} />
                     <Route path='/welcome/:username' element={<WelcomeComponent />} />
                     <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='/logout' element={<LogoutComponent />} />
                     <Route path='*' element={<ErrorComponent />} />
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
         </div>
     )
@@ -107,10 +110,10 @@ function ListTodosComponent() {
                 ]
     
     return (
-        <div className="ListTodosComponent">
+        <div className="container">
             <h1>Things you want to do!</h1>
             <div>
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
                             <td>ID</td>
@@ -134,6 +137,51 @@ function ListTodosComponent() {
                         }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.github.com/miguelfcaiado">GitHub</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item"><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item"><Link className="nav-link" to="/logout">Logout</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/">Login</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                Your Footer
+            </div>
+        </footer>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our app. Come back soon!
             </div>
         </div>
     )
