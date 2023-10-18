ea8d2977112353dd8b94fdc2057a8ae641e583c5
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-10-18 21:43:37 +0100

    Call spring boot hello world rest api

--- first-npm-project/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ first-npm-project/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,34 @@
 import { useParams, Link } from "react-router-dom"
+import axios from "axios"
 
 function WelcomeComponent() {
     
     const {username} = useParams()
+
+    function callHelloWorldRestApi() {
+        axios.get('http://localhost:8080/hello-world').then(
+            (response) => successfullResponse())
+        .catch((error) => errorResponse(error))
+        .finally(() => console.log('cleanup'))
+
+    }
+
+    function successfullResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div>
             <h1>Welcome {username}</h1>
             <div className="WelcomeComponent">
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5"  onClick={callHelloWorldRestApi}>Call Hello World</button>
             </div>
         </div>
     )
