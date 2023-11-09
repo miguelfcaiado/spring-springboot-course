e79542acae6450cbdcd86dd9c3fea754717ae165
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-11-09 19:44:38 +0000

    Refactoring rest api invocation

--- first-npm-project/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ first-npm-project/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,20 +1,28 @@
 import { useParams, Link } from "react-router-dom"
-import axios from "axios"
+import { useState } from "react"
+import { retrieveHelloWorldBean } from "./api/HelloWorldApiService"
 
 function WelcomeComponent() {
     
     const {username} = useParams()
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi() {
-        axios.get('http://localhost:8080/hello-world').then(
-            (response) => successfullResponse())
+        // axios.get('http://localhost:8080/hello-world').then(
+        //     (response) => successfullResponse(response))
+        // .catch((error) => errorResponse(error))
+        // .finally(() => console.log('cleanup'))
+        
+        retrieveHelloWorldBean()
+        .then((response) => successfullResponse(response))
         .catch((error) => errorResponse(error))
         .finally(() => console.log('cleanup'))
-
     }
-
+    
     function successfullResponse(response) {
         console.log(response)
+        setMessage(response.data.message)
     }
 
     function errorResponse(error) {
@@ -30,6 +38,7 @@ function WelcomeComponent() {
             <div>
                 <button className="btn btn-success m-5"  onClick={callHelloWorldRestApi}>Call Hello World</button>
             </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
