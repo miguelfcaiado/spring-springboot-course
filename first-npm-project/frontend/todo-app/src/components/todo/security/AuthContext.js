ce699e2f14c48e397662aa6b02204da68d73d66d
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-11-22 11:23:09 +0000

    Setting up axios interceptor to add authentication header

--- first-npm-project/frontend/todo-app/src/components/todo/security/AuthContext.js
+++ first-npm-project/frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext } from "react";
 import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
+import { apiClient } from "../api/ApiClient";
 
 const AuthContext = createContext()
 
@@ -36,6 +37,14 @@ export default function AuthProvider({ children }) {
                 setAuthenticated(true)
                 setUsername(username)
                 setToken(baToken)
+
+                apiClient.interceptors.request.use(
+                    (config) => {
+                        config.headers.Authorization = baToken
+                        return config
+                    }
+                )
+
                 return true
             }
             else {
