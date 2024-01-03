37ada905cef65f36d1eb3121bb9b8f686a67f76b
miguelfcaiado <caiado.miguelwork@gmail.com>
2024-01-03 16:53:43 +0000

    Assert methods

---
+++ junit-in-5-steps/test/com/in28minutes/junit/MyAssertTest.java
@@ -0,0 +1,26 @@
+package com.in28minutes.junit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class MyAssertTest {
+
+	List<String> todos = Arrays.asList("AWS", "Azure", "DevOps");
+	
+	@Test
+	void test() {
+		
+		boolean test = todos.contains("AWS");
+		
+		//assertEquals(true, test);
+		assertTrue(test);
+		
+		assertEquals(3, todos.size());
+	}
+
+}
